b20b5011f97e641c9ea6143eed1c418fc9b5eb4d
Merlin Schumacher <merlin.schumacher@gmail.com>
2017-11-01 00:52:27 +0100

    Added ESP32 library and example

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-Library EPD E-Paper
+ESP32 EPD E-Paper Library
 =======
 
-Library Arduino 4.3inch e-Paper for Waveshare 4.3 inch E-Paper Display Module
+ESP32 Arduino Library for the 4.3inch e-Paper display by Waveshare
 
-Tutorial of use in [Instructable](http://www.instructables.com/id/E-paper-Display-With-ArduinoESP8266/)
+See wiring.jpg and wiring-usb.jpg for information on how to connect the Display
 
-Buy Module Display in [GearBest](http://goo.gl/51y8Hq  )
+![Wiring for the ESP32](wiring.JPG)
+
+This repository combines a modified version of the esp8266 epd-library by [G6EJD](https://github.com/G6EJD/ESP32-8266-ePaper-Serial-UART-Wx-Display) with the example made by [sabas1080](https://github.com/sabas1080/LibraryEPD).
+
+Instead of using RX0/TX0 this library and example use RX2/TX2 to communicate with the display.
--- epd.h
+++ epd.h
@@ -1,7 +1,7 @@
 /*********************************************************************************************************
 *
 * File                : epd.h
-* Hardware Environment:
+* Hardware Environment: 
 * Build Environment   : RealView MDK-ARM  Version: 4.74
 * Version             : V1.0
 * By                  : V
@@ -10,20 +10,12 @@
 *                                       http://www.waveshare.net
 *                                          All Rights Reserved
 *
-*
-*																		Modified for Andres Sabas
-*																		s@theinventorhouse.org
-*																				17 April 2016
-*									Change pin wakeup an reset from Scketch for work wiht ESP8266
-*                 Creative Commons Atribución-CompartirIgual 4.0 Internacional
-*
-*
 *********************************************************************************************************/
 #ifndef    EPD_H
   #define  EPD_H
+	
 
-
-#define    CMD_SIZE                           512
+#define    CMD_SIZE                           512	
 
 /*
 frame format
@@ -50,16 +42,14 @@ command define
 #define    CMD_SET_BAUD                       0x01                                                     //set baud
 #define    CMD_READ_BAUD                      0x02                                                     //read baud
 #define    CMD_MEMORYMODE                     0x07                                                     //set memory mode
-#define    CMD_STOPMODE                       0x08                                                     //enter stop mode
+#define    CMD_STOPMODE                       0x08                                                     //enter stop mode 
 #define    CMD_UPDATE                         0x0A                                                     //update
 #define    CMD_SCREEN_ROTATION                0x0D                                                     //set screen rotation
 #define    CMD_LOAD_FONT                      0x0E                                                     //load font
 #define    CMD_LOAD_PIC                       0x0F                                                     //load picture
-
 #define    CMD_SET_COLOR                      0x10                                                     //set color
 #define    CMD_SET_EN_FONT                    0x1E                                                     //set english font
 #define    CMD_SET_CH_FONT                    0x1F                                                     //set chinese font
-
 #define    CMD_DRAW_PIXEL                     0x20                                                     //set pixel
 #define    CMD_DRAW_LINE                      0x22                                                     //draw line
 #define    CMD_FILL_RECT                      0x24                                                     //fill rectangle
@@ -68,11 +58,8 @@ command define
 #define    CMD_DRAW_TRIANGLE                  0x28                                                     //draw triangle
 #define    CMD_FILL_TRIANGLE                  0x29                                                     //fill triangle
 #define    CMD_CLEAR                          0x2E                                                     //clear screen use back color
-
 #define    CMD_DRAW_STRING                    0x30                                                     //draw string
-
 #define    CMD_DRAW_BITMAP                    0x70                                                     //draw bitmap
-
 
 /*
 FONT
@@ -80,7 +67,7 @@ FONT
 #define    GBK32                              0x01
 #define    GBK48                              0x02
 #define    GBK64                              0x03
-
+	
 #define    ASCII32                            0x01
 #define    ASCII48                            0x02
 #define    ASCII64                            0x03
@@ -97,21 +84,20 @@ Memory Mode
 set screen rotation
 */
 #define    EPD_NORMAL                         0                                                        //screen normal
-#define    EPD_INVERSION                      1                                                        //screen inversion
+#define    EPD_INVERSION                      1                                                        //screen inversion      
 
 
-void epd_init(int wake_up, int reset);
-void epd_reset(int reset);
-void epd_wakeup(int wake_up);
+void epd_init(void);
+void epd_reset(void);
+void epd_wakeup(void);
 
 void epd_handshake(void);
 void epd_set_memory(unsigned char mode);
 void epd_enter_stopmode(void);
-void epd_udpate(void);
+void epd_update(void);
 void epd_screen_rotation(unsigned char mode);
 void epd_load_font(void);
 void epd_load_pic(void);
-
 
 void epd_set_color(unsigned char color, unsigned char bkcolor);
 void epd_set_en_font(unsigned char font);
@@ -131,6 +117,34 @@ void epd_disp_string(const void * p, int x0, int y0);
 
 void epd_disp_bitmap(const void * p, int x0, int y0);
 
-
-
 #endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
